da8296d21f280c0eb1da9bd649ddbd1e644ab511
Maksim Moryakov <maksim.moryakov@gmail.com>
2022-09-24 13:49:07 +0300

    Решение домашней задачи №2 и 3

---
+++ HomeWork1/Exam2.py
@@ -0,0 +1,16 @@
+# Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+from pickle import FALSE, TRUE
+
+
+list = [True, False]
+for X in list:
+    for Y in list:
+        for Z in list:
+            a = not (X or Y or Z)
+            b = (not X) and (not Y) and (not Z)
+            print(f'Утверждение: "¬({X}" ⋁ "{Y}" "{Z})" = ¬"{X}" ⋀ ¬"{Y}" ⋀ ¬"{Z}"')
+            if a == b:
+                print('Утверждение истинное')
+            else:
+                print('Утверждение ложное')
