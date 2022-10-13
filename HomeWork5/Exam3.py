c747008d5b2449eb0029b48e1e6dd9237fd9eba1
Maksim Moryakov <79351987+maksim-moryakov@users.noreply.github.com>
2022-10-13 11:08:23 +0300

    Крестики-нолики

--- HomeWork5/Exam3.py
+++ HomeWork5/Exam3.py
@@ -13,8 +13,64 @@ def draw_board(board):
 
 draw_board(board)
 
-https://it-black.ru/igra-krestiki-noliki-dlja-dvuh-igrokov-na-python/
 
+# функция принимает ввод пользователя
+
+def take_input(player):
+    valid = False
+    while not valid:
+        player_answer = input('Куда поставит ' + player +'?')
+        try:
+            player_answer = input(player_answer)
+        except:
+            print('Введено не число.')
+            continue
+        if player_answer >= 1 and player_answer <= 9:
+            if (str(board[player_answer-1]) not in 'XO'):
+                board[player_answer-1] = player
+                valid = True
+            else:
+                print('Это место занято.')
+        else:
+            print('Неверное число, нужно ввести число от 1 до 9')
+
+
+# функция проверки игрового поля
+
+def check_win(board):
+    win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+    for each in win_coord:
+        if board[each[0]] == board[each[1] == board[2]]:
+            return board[each[0]]
+    return False
+
+
+#  основная функция игры
+
+def main(board):
+    count = 0
+    win = False
+    while not win:
+        draw_board(board)
+        if count % 2 == 0:
+            take_input('X')
+        else:
+            take_input('O')
+        count += 1
+        if count > 4:
+            tmp = check_win(board)
+            if tmp:
+                print(tmp, 'Win!')
+                win = True
+                break
+        if count == 9:
+            print('Draw!')
+            break
+    draw_board(board)
+main(board)
+
+
+input('Press Enter to exit!')
 
 exit()
 X = "X"
